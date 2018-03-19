5b1d862a8e6048d31367ab436aec3f9b74b10b7f
Constantin Bugneac <constantin.bugneac@tesglobal.com>
2018-03-19 17:38:27 +0000

    added metricbeat

--- assets.tf
+++ assets.tf
@@ -1,6 +1,11 @@
 variable "version" {
   description = "Version of ES and Kibana to use"
-  default     = "6.1.3"
+  default     = "6.2.2"
+}
+
+variable "elastic_password" {
+  description = "Default Elasticsearch password"
+  default     = "changeme"
 }
 
 # Root CA
@@ -133,6 +138,40 @@ resource "tls_locally_signed_cert" "elasticsearch_kibana_client" {
     "digital_signature",
     "key_encipherment",
     "client_auth",
+    "server_auth",
+  ]
+}
+
+# Metricbeat
+resource "tls_private_key" "metricbeat_client_key" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "metricbeat_client_req" {
+  key_algorithm   = "${tls_private_key.metricbeat_client_key.algorithm}"
+  private_key_pem = "${tls_private_key.metricbeat_client_key.private_key_pem}"
+
+  subject {
+    common_name  = "metricbeat"
+    organization = "elasticsearch"
+  }
+}
+
+resource "tls_locally_signed_cert" "metricbeat_client_cert" {
+  cert_request_pem = "${tls_cert_request.metricbeat_client_req.cert_request_pem}"
+
+  ca_key_algorithm   = "${tls_private_key.intermediate_ca.algorithm}"
+  ca_private_key_pem = "${tls_private_key.intermediate_ca.private_key_pem}"
+  ca_cert_pem        = "${tls_locally_signed_cert.intermediate_ca.cert_pem}"
+
+  validity_period_hours = 8760
+  early_renewal_hours   = 2160
+
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "client_auth",
   ]
 }
 
@@ -151,6 +190,7 @@ locals {
     )))}"
 }
 
+# Elasticsearch
 resource "local_file" "ssl_elasticsearch_ca_crt" {
   content  = "${local.ca_cert_chain_pem}"
   filename = "${path.module}/ssl/elasticsearch/ca.crt"
@@ -166,6 +206,7 @@ resource "local_file" "ssl_elasticsearch_server_key" {
   filename = "${path.module}/ssl/elasticsearch/server.key"
 }
 
+# Kibana
 resource "local_file" "ssl_kibana_ca_crt" {
   content  = "${local.ca_cert_chain_pem}"
   filename = "${path.module}/ssl/kibana/ca.crt"
@@ -181,9 +222,62 @@ resource "local_file" "ssl_kibana_client_key" {
   filename = "${path.module}/ssl/kibana/client.key"
 }
 
+# Metricbeat
+resource "local_file" "ssl_metricbeat_ca_crt" {
+  content  = "${local.ca_cert_chain_pem}"
+  filename = "${path.module}/ssl/metricbeat/ca.crt"
+}
+
+resource "local_file" "ssl_metricbeat_client_crt" {
+  content  = "${tls_locally_signed_cert.metricbeat_client_cert.cert_pem}"
+  filename = "${path.module}/ssl/metricbeat/client.crt"
+}
+
+resource "local_file" "ssl_metricbeat_client_key" {
+  content  = "${tls_private_key.metricbeat_client_key.private_key_pem}"
+  filename = "${path.module}/ssl/metricbeat/client.key"
+}
+
+resource "local_file" "metricbeat_yml" {
+  content = <<EOF
+metricbeat.config.modules:
+  # Glob pattern for configuration loading
+  path: $${path.config}/modules.d/*.yml
+
+  # Set to true to enable config reloading
+  reload.enabled: true
+
+  # Period on which files under path should be checked for changes
+  reload.period: 10s
+
+name: metricbeat
+
+fields:
+  environment: local
+
+processors:
+- add_cloud_metadata: ~
+
+setup.kibana.host: "http://kibana:5601"
+setup.kibana.username: elastic
+setup.kibana.password: "${var.elastic_password}"
+
+output.elasticsearch:
+  hosts: ["https://elasticsearch:9200"]
+  ssl.certificate_authorities: ["/etc/pki/ca.crt"]
+  ssl.certificate: "/etc/pki/client.crt"
+  ssl.key: "/etc/pki/client.key"
+  ssl.supported_protocols: ["TLSv1.2"]
+  ssl.verification_mode: full
+  enabled: true
+EOF
+
+  filename = "${path.module}/assets/metricbeat.yml"
+}
+
 resource "local_file" "docker_compose_yml" {
   content = <<EOF
-version: '2.2'
+version: '3.3'
 services:
   elasticsearch:
     image: docker.elastic.co/elasticsearch/elasticsearch:${var.version}
@@ -191,10 +285,12 @@ services:
       - ./ssl/elasticsearch:/etc/ssl/elasticsearch
       - ./assets/java.policy:/usr/share/elasticsearch/java.policy
       - ./assets/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml
+      - ./assets/roles.yml:/usr/share/elasticsearch/config/x-pack/roles.yml:ro
+      - ./assets/role_mapping.yml:/usr/share/elasticsearch/config/x-pack/role_mapping.yml:ro
       - elasticsearch-data:/usr/share/elasticsearch/data
     environment:
       __CONFIG_ID: "${local.config_id}"
-      ELASTIC_PASSWORD: "changeme"
+      ELASTIC_PASSWORD: "${var.elastic_password}"
       ES_JAVA_OPTS: "-Djava.security.policy=/usr/share/elasticsearch/java.policy"
       "discovery.type": "single-node"
     ports:
@@ -222,6 +318,16 @@ services:
       ELASTICSEARCH_SSL_VERIFICATIONMODE: full
     ports:
       - 5601:5601
+    networks:
+      - elastic-x-pack
+  metricbeat:
+    image: docker.elastic.co/beats/metricbeat:${var.version}
+    volumes:
+      - ./ssl/metricbeat:/etc/pki:ro
+      - ./assets/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro
+      - ./assets/modules.d:/usr/share/metricbeat/modules.d:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+    user: root
     networks:
       - elastic-x-pack
 volumes:
